c94c74bb88af27107a29e10714b860eeecf86cac
viswasaicb <viswasaicb@gmail.com>
2024-01-02 00:07:11 +0530

    printed palindrome msg

--- app.py
+++ app.py
@@ -6,3 +6,12 @@ print(type(a),type(b))
 print(a==a[::-1]) #'rader'=='rader'-True #start index - 0, stop index - 4, step - -1
 #It has to start from stop index to start index
 print(b==b[::-1]) #'madhu'=='uhdam'-False
+if a==a[::-1]:
+    print(a+'is a palindrome')
+else:
+    print(a+'is not a palindrome')
+
+if b==b[::-1]:
+    print(b+'is a palindrome')
+else:
+    print(b+'is not a palindrome')
