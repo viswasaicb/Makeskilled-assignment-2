d6c6253448c3375fdc56e7b966885ab8ad16356e
viswasaicb <viswasaicb@gmail.com>
2024-01-01 23:33:07 +0530

    modified hello world to palindrome string

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print('hello world')
+a='rader'
+b='madhu'
+print(type(a),type(b))
+
+#Check palindrome
+print(a==a[::-1]) #'rader'=='rader'-True #start index - 0, stop index - 4, step - -1
+#It has to start from stop index to start index
+print(b==b[::-1]) #'madhu'=='uhdam'-False
